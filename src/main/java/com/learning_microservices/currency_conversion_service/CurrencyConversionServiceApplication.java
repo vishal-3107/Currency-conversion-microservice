1c88431b637f6dd749e925ad2a1bb2974f5455c4
vishal-3107 <vishalkumarsingh028@gmail.com>
2025-03-25 03:08:11 +0530

    "added currency-conversion-service"

---
+++ src/main/java/com/learning_microservices/currency_conversion_service/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.learning_microservices.currency_conversion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
